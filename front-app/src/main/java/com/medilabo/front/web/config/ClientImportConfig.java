f55c840f01ff676653ddfe6c3a31ff6eb7e20865
alcan <alcan@example.com>
2026-03-26 14:02:06 +0100

    MAJ

--- front-app/src/main/java/com/medilabo/front/web/config/ClientImportConfig.java
+++
@@ -1,9 +0,0 @@
-package com.medilabo.front.web.config;
-
-import com.medilabo.libs.client.config.ClientAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
-@Configuration
-@Import(ClientAutoConfiguration.class)
-public class ClientImportConfig {}
