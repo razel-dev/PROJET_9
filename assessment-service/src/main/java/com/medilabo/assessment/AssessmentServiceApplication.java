e2ebe217054fae61b303a1d79971d6d679a39da5
alcan <alcan@example.com>
2026-04-25 16:11:52 +0200

    initialiser l’application Spring Boot et activer OpenFeign - Ajoute la classe d’entrée AssessmentServiceApplication avec @SpringBootApplication - Active les clients Feign via @EnableFeignClients sur com.medilabo.assessment.client - Démarre l’app avec SpringApplication.run(...) pour charger le contexte et l’auto-config

--- assessment-service/src/main/java/com/medilabo/assessment/AssessmentServiceApplication.java
+++ assessment-service/src/main/java/com/medilabo/assessment/AssessmentServiceApplication.java
@@ -2,12 +2,12 @@ package com.medilabo.assessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.medilabo.assessment.client")
 public class AssessmentServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AssessmentServiceApplication.class, args);
     }
-
 }
